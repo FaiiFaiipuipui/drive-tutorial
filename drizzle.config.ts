15bc39af627e8b7041996405ab37bea03c663689
FaiiFaiipuipui <pakarnpahulo2003@gmail.com>
2025-07-29 15:27:15 +0700

    Database added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { type Config } from "drizzle-kit";
 
 import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "singlestore",
+  tablesFilter: ["drive_tutorial_*"],
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
   },
-  tablesFilter: ["drive-tutorial_*"],
 } satisfies Config;
